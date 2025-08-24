91cf84ebace4432a88b3c177c66b6b1e097b4acf
bichipham <ptbich308@gmail.com>
2025-08-25 01:45:10 +0700

    - generate data

---
+++ SequelizeAppfood/src/models/sequelize-auto/like_res.js
@@ -0,0 +1,46 @@
+import _sequelize from 'sequelize';
+const { Model, Sequelize } = _sequelize;
+
+export default class like_res extends Model {
+  static init(sequelize, DataTypes) {
+  return super.init({
+    user_id: {
+      type: DataTypes.INTEGER,
+      allowNull: true,
+      references: {
+        model: 'user',
+        key: 'user_id'
+      }
+    },
+    res_id: {
+      type: DataTypes.INTEGER,
+      allowNull: true,
+      references: {
+        model: 'restaurant',
+        key: 'res_id'
+      }
+    }
+  }, {
+    sequelize,
+    tableName: 'like_res',
+    timestamps: false,
+    freezeTableName: true,
+    indexes: [
+      {
+        name: "user_id",
+        using: "BTREE",
+        fields: [
+          { name: "user_id" },
+        ]
+      },
+      {
+        name: "res_id",
+        using: "BTREE",
+        fields: [
+          { name: "res_id" },
+        ]
+      },
+    ]
+  });
+  }
+}
