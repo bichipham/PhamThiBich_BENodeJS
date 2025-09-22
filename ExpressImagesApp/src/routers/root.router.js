39880c87b5cfa1128717765a6561332dcd58671e
bichipham <ptbich308@gmail.com>
2025-09-23 00:45:14 +0700

    + add project

---
+++ ExpressImagesApp/src/routers/root.router.js
@@ -0,0 +1,8 @@
+import express from "express";
+
+const rootRouter = express.Router();
+// rootRouter.use("/restaurant", restaurantRouter);
+// rootRouter.use("/user", userRouter);
+
+
+export default rootRouter;
