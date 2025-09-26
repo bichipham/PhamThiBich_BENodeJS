cc6b5aef8e93e445bf69606538ae6cc13580c78d
bichipham <ptbich308@gmail.com>
2025-09-26 22:43:44 +0700

    - add readme

--- ExpressImagesApp/src/routers/image.router.js
+++ ExpressImagesApp/src/routers/image.router.js
@@ -4,7 +4,6 @@ import { protect } from '../common/middlewares/protect.middleware';
 
 const imageRouter = express.Router();
 
-// Tạo route CRUD
 imageRouter.get('/', protect, imageController.findAll);
 imageRouter.get('/:id',protect, imageController.findOne);
 imageRouter.get('/:id/comment',protect, imageController.getComment);
